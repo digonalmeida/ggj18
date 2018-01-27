93301a7d777b7b674756d39620af90e74bc57527
Rodrigo Nietiedt de Almeida <rodrigonietiedt@gmail.com>
2018-01-27 09:37:36 -0200

    curando e state machine dos pacientes

--- Assets/Scripts/Paciente.cs
+++ Assets/Scripts/Paciente.cs
@@ -9,16 +9,80 @@ public class Paciente : MonoBehaviour {
     public float maxDistance;
     public float infectionLevel;
     public bool isSick = false;
-
-    SpriteRenderer spriteRenderer;
+    
+    public SpriteRenderer spriteRenderer;
 
     private float infectionTime = 0f;
     private float infectionCooldown = 2f;
+    public float infectionPerSecond = 1;
+    public float healPerSecond = 2;
 
-    void Start () {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+    private bool _lastDirection = true;
+
+    public float hospitalLinePos;
+    public bool bateVolta = false;
+    
+
+    // Máquina de estados do paciente
+    /*
+    walking -> stunned (OnDragged -- chamado por DragAndDrop.cs)
+    stunned -> hospital/walking (OnDragged -- chamado por DragAndDrop.cs)
+    */
+
+    public enum State {
+        WALKING,
+        STUNNED,
+        HOSPITAL
+    }
+    public State state;
+    void Start()
+    {
+
     }
 
+
+    void Update()
+    {
+        switch(state)
+        {
+            case State.WALKING:
+                UpdateWalking();
+                break;
+            case State.HOSPITAL:
+                UpdateHospital();
+                break;
+        }
+    }
+
+    void UpdateHospital()
+    {
+        infectionLevel -= healPerSecond * Time.deltaTime;
+        if (infectionLevel <= -10) infectionLevel = 10;
+        UpdateInfectionIndicator();
+       
+    }
+    void setState(State newState)
+    {
+        state = newState;
+        switch (state)
+        {
+        }
+    }
+    public void OnDragged()
+    {
+        setState(State.STUNNED);
+    }
+    public void OnDropped()
+    {
+        if(transform.position.y > 0)
+        {
+            setState(State.WALKING);
+        }
+        else
+        {
+            setState(State.HOSPITAL);
+        }
+    }
     void Awake ()
     {
         infectionLevel = Random.Range(0, 10f);
@@ -41,6 +105,10 @@ public class Paciente : MonoBehaviour {
 
     void OnTriggerEnter2D (Collider2D other)
     {
+        if(state != State.WALKING)
+        {
+            return;
+        }
         // Se estou dentro do raio de um infectado, me infecte mais rápido. NÃO FUNCIONA
         if (other.gameObject.GetComponent<Paciente>() != null)
         {
@@ -49,23 +117,61 @@ public class Paciente : MonoBehaviour {
             if ((direction != otherDirection) && other.tag != "Trigger" && (Time.time > infectionTime + infectionCooldown))
             {
                 Debug.Log("Estou em um trigger!? " + gameObject.tag + "->" + other.gameObject.tag);
-                infectionLevel += 1f;
+                infectionLevel += 5f;
                 infectionTime = Time.time;
             }
         }
     }
 
-    void Update () {
+    
+    void updateDirection()
+    {
+        if (!direction) // Significa que spawnei na direita. Mover para a esquerda.
+        {
+            Vector3 newScale = transform.localScale;
+            newScale.x = -Mathf.Abs(newScale.x);
+            transform.localScale = newScale;
 
+        }
+        else // Significa que spawnei na esquerda. Mover para a direita.
+        {
+            Vector3 newScale = transform.localScale;
+            newScale.x = Mathf.Abs(newScale.x);
+            transform.Translate(Vector3.right * Time.deltaTime * (walkSpeed + Random.Range(0f, 2f)));
+        }
+    }
+
+    void UpdateInfectionIndicator()
+    {
+        float l = infectionLevel;
+        if(l < 0)
+        {
+            l = 0;
+        }
+
+        Color c = spriteRenderer.color;
+        c.a = l * 0.1f;
+        spriteRenderer.color = c;
+    }
+    void UpdateWalking(){
+        
+        if(direction != _lastDirection)
+        {
+            updateDirection();
+            _lastDirection = direction;
+        }
         // Mais infectado = mais verde
         if (infectionLevel > 10) infectionLevel = 10;
-        spriteRenderer.color = new Color(infectionLevel * 0.1f, 0f, 0f);
+        if (infectionLevel < 0) infectionLevel = 0;
 
-        infectionLevel += 0.1f * Time.deltaTime;
+        UpdateInfectionIndicator();
+
+        infectionLevel += infectionPerSecond * Time.deltaTime;
 
         if (!direction) // Significa que spawnei na direita. Mover para a esquerda.
         {
             transform.Translate(Vector3.left * Time.deltaTime * (walkSpeed + Random.Range(0f, 2f)));
+            
         }
         else // Significa que spawnei na esquerda. Mover para a direita.
         {
@@ -82,5 +188,18 @@ public class Paciente : MonoBehaviour {
             Debug.Log("Estou fora da tela. Ativando suicídio.");
             Destroy(gameObject);
         }
+
     }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+
+        if (coll.collider.tag == "Wall")
+        {
+            direction = !direction;
+        }
+        updateDirection();
+    }
+
+    
 }
--- Assets/Scripts/Teste/TestesRodrigo.cs
+++ Assets/Scripts/Teste/TestesRodrigo.cs
@@ -12,6 +12,14 @@ public class TestesRodrigo : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        transform.position += (Vector3) Vector2.right * (walkVelocity * Time.deltaTime);
 	}
+
+    void OnMouseDown()
+    {
+    }
+
+    void OnMouseUp()
+    {
+
+    }
 }
