ec56d5f3c75097ebc2e767ea8cfe24119cc722a9
Pedro Lumertz <pedrinhoulumertz@hotmail.com>
2018-01-27 05:22:52 -0200

    Implementação inicial da infecção entre pacientes

--- Assets/Scripts/Paciente.cs
+++ Assets/Scripts/Paciente.cs
@@ -7,6 +7,8 @@ public class Paciente : MonoBehaviour {
     public float walkSpeed;
     public bool direction;
     public float maxDistance;
+    public float infectionLevel;
+    public bool isSick = false;    
 
     void Start () {
         
@@ -14,6 +16,14 @@ public class Paciente : MonoBehaviour {
 
     void Awake ()
     {
+        infectionLevel = Random.Range(0, 10f);
+
+        if (infectionLevel >= 6)
+        {
+            isSick = true;
+        }
+
+        // Define, ao iniciar, o lado que ele se movimentará
         if (transform.position.x > 0)
         {
             direction = false;
@@ -23,9 +33,21 @@ public class Paciente : MonoBehaviour {
             direction = true;
         }
     }
-	
-	void Update () {
-		if (!direction) // Significa que spawnei na direita. Mover para a esquerda.
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        // Se estou dentro do raio de um infectado, me infecte mais rápido. NÃO FUNCIONA
+        if(direction && other.tag != "Trigger")
+        {
+            Debug.Log("Estou em um trigger!? " + gameObject.tag + "->" + other.gameObject.tag);
+            infectionLevel += 0.5f;
+        }
+    }
+
+    void Update () {
+        infectionLevel += 0.1f * Time.deltaTime;
+
+        if (!direction) // Significa que spawnei na direita. Mover para a esquerda.
         {
             transform.Translate(Vector3.left * Time.deltaTime * (walkSpeed + Random.Range(0f, 2f)));
         }
@@ -37,7 +59,9 @@ public class Paciente : MonoBehaviour {
         // Pequena aleatorizada no movimento vertical
         transform.Translate(Vector3.up * Time.deltaTime * (Random.Range(-1f, 1f)));
 
-        if (transform.position.x < -maxDistance || transform.position.x > 9.5f)
+
+        // É destruído se sai da tela
+        if (transform.position.x < -maxDistance || transform.position.x > maxDistance)
         {
             Debug.Log("Estou fora da tela. Ativando suicídio.");
             Destroy(gameObject);
