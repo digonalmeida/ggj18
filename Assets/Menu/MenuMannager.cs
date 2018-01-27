0676fd7c2fc95faee4faa2ad6269da5c4a44973c
Henrique Manfroi <hmanfroi@hotmail.com>
2018-01-27 08:12:10 -0200

    Credits

--- Assets/Menu/MenuMannager.cs
+++ Assets/Menu/MenuMannager.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuMannager : MonoBehaviour {
 
+	public Text scoreText;
+	public int score;
 	// Use this for initialization
 	void Start () {
-		
+		this.score = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		score = (int) Time.timeSinceLevelLoad;
+		scoreText.text = "" + score;
 	}
 }
