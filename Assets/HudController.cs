b10e567e7fff3dea72b7808b17885c58a0118f38
Rodrigo Nietiedt de Almeida <rodrigonietiedt@gmail.com>
2018-01-28 06:10:53 -0200

    hud

---
+++ Assets/HudController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudController : MonoBehaviour {
+    public Text zombiesText;
+    public Text savesText;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int maxZombies = GameManager.Instance.maxZombie;
+        int zombies = GameManager.Instance.contZombie;
+        int saves = GameManager.Instance.scoreSaved;
+        zombiesText.text = zombies.ToString() + "/" + maxZombies;
+        savesText.text = saves.ToString() ;
+	}
+}
